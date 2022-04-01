954083b0e771e66cc7ab400c86dcf5da0f79cbb2
FryTechTips <frytechtips@gmail.com>
2022-04-01 11:39:40 -0400

    Very alpha ZM config.

---
+++ !start_zm_server.bat
@@ -0,0 +1,25 @@
+@echo off
+::Paste the server key from https://platform.plutonium.pw/serverkeys here
+set key=placeServerKeyHere
+::Name of the config file the server should use. (default: server_zm.cfg)
+set cfg=server_zm.cfg
+::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
+set name=T5 Zombies 1
+::Port used by the server (default: 28960)
+set port=28961
+::Mod name (default "")
+set mod=""
+::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
+set gamepath=%cd%
+
+title PlutoniumT5 ZM - %name% - Server restarter
+echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
+echo Server "%name%" will load %cfg% and listen on port %port% UDP!
+echo To shut down the server close this window first!
+echo (%date%)  -  (%time%) %name% server start.
+
+cd /D %LOCALAPPDATA%\Plutonium
+:server
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t5sp "%gamepath%" -dedicated +set key %key% +set fs_game %mod% +sv_config %cfg% +set net_port %port% +map_rotate
+echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
+goto server
